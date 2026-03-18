2e8f0964bfb391a96b789912e9fe0a29adcdf237
saipothireddy18 <saipothireddy18@gmail.com>
2026-03-18 09:43:16 +0530

    Add files via upload

---
+++ HelloAppUC2.java
@@ -0,0 +1,22 @@
+/**
+ * HelloApp - UC2 - Display Hello with Command-Line Argument
+ * @author SaiPothireddy
+ * @version 2.0
+ */
+public class HelloAppUC2 {
+    public static void main(String[] args) {
+        
+        // Check if user provided a name as command-line argument
+        if (args.length > 0) {
+            // Get the name from command-line argument
+            String name = args[0];
+            
+            // Display personalized greeting
+            System.out.println("Hello, " + name);
+        } else {
+            // No argument provided, display default
+            System.out.println("Hello World");
+        }
+        
+    }
+}
